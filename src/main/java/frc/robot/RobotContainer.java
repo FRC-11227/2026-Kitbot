d419d7d2e753db36e71bb14f0e175f1f513bd316
Charles Stuart <charlieastuart@gmail.com>
2026-02-13 18:12:12 -0500

    Update libraries and fix errors for robot container on branch charles

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -79,10 +79,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-<<<<<<< HEAD
     return Autos.driveInASquare(m_DrivetrainSubsystem);
-=======
-    return Autos.spin180(m_DrivetrainSubsystem);
->>>>>>> origin/Zoey
-  }
-}
+
+}}
