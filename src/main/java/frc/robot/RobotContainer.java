6e1d1478f392200494e83db3aa4813fef34daf89
mwhatman-robot <whatman.matthew@gmail.com>
2026-03-13 13:33:19 -0400

    First Commit

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -63,7 +63,8 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
+    // An example command will be run in autonomousj
     return Autos.runFiveSec(m_DrivetrainSubsystem);
   }
 }
+//test
